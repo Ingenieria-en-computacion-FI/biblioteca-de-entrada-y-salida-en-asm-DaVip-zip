685e195431c2f7933dab3eb159ec6beb8c31a9d0
DaVip-zip <davidlopcar2005@gmail.com>
2026-03-14 15:44:18 -0600

    Update scan_string.asm

--- src/scan_string.asm
+++ src/scan_string.asm
@@ -1,23 +1,39 @@
-SECTION .text
 global scan_string
 
-; ---------------------------------
-; scan_string
-; Entrada:
-;   EAX = buffer destino
-;   EBX = tamaño máximo
-; ---------------------------------
-
+section .text
+; Lee una cadena (EAX = destino, EBX = tamaño máximo) y la termina en cero.
 scan_string:
-
     push ebp
     mov ebp, esp
-
-    ; TODO:
-    ; 1. syscall read
-    ; 2. guardar en buffer
-    ; 3. agregar terminador 0
-
+    
+    push ebx
+    push ecx
+    push edx
+    
+    mov ecx, eax        ; Dirección del buffer
+    mov edx, ebx        ; Tamaño máximo
+    dec edx             ; Reservar espacio para el cero
+    
+    mov eax, 3          ; sys_read
+    mov ebx, 0          ; stdin
+    int 0x80
+    
+    cmp eax, 0
+    jle .fin
+    
+    cmp byte [ecx + eax - 1], 10
+    jne .poner_cero
+    mov byte [ecx + eax - 1], 0
+    jmp .fin
+    
+.poner_cero:
+    mov byte [ecx + eax], 0
+    
+.fin:
+    pop edx
+    pop ecx
+    pop ebx
+    
     mov esp, ebp
     pop ebp
     ret
